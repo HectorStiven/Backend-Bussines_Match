632774ec4dfebf3e0d2a331259a46d370b9fe940
HectorStiven <vigoyahector90@gmail.com>
2024-11-12 19:46:52 -0500

    zxxxxxxxxxxxxxxxxxxx

--- bussines_match/settings.py
+++ bussines_match/settings.py
@@ -27,6 +27,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.getenv('BIA_EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('BIA_EMAIL_HOST_PASSWORD')
+
 
 # Application definition
 
